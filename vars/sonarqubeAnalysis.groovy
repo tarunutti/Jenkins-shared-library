b49bb86edfe7a034e36de532e3abfa49dc6740d2
tarunutti <uttitarunteja@gmail.com>
2024-03-25 17:24:14 +0530

    fith commit

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,5 +1,5 @@
 def call() {
-    withSonarQubeEnv('sonar-server') {
+    withSonarQubeEnv('SonarQube') {
         sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Youtube -Dsonar.projectKey=Youtube '''
     }
 }
